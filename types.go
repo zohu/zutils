bf22a307fe5469f4bafb80a53fe1efb631b080ad
oak <ch@beituyun.com>
2023-04-18 14:05:09 +0800

    fix：aes加密

--- types.go
+++ types.go
@@ -1,8 +1,7 @@
 package zutils
 
 type Int interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 type Float interface {
 	~float32 | ~float64
